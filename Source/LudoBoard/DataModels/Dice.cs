624667d7c2d74603915c572c1537188b4e10b9b4
PROGRAMMERINGEN\Patrik <patrikpersson45@gmail.com>
2021-03-31 14:57:17 +0200

    Added projects and classes
    Made some setups to properies in ceveral classes to.

---
+++ Source/LudoBoard/DataModels/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoBoard.DataModels
+{
+    public class Dice
+    {        
+        public int RollDice()
+        {
+            Random diceValue = new Random();
+            int DiceValue = diceValue.Next(1, 7);
+            return DiceValue;            
+        }
+    }
+
+}
---
+++ Source/LudoGameEngine/Program.cs
@@ -0,0 +1,18 @@
+﻿using LudoBoard.DataModels;
+using System;
+
+namespace LudoGameEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var runMenu = new Menu();
+            
+            var rolledDice = new Dice();
+            rolledDice.RollDice();
+
+            Console.WriteLine($"{rolledDice.RollDice()}");
+        }
+    }
+}
