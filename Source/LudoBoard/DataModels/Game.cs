dc8d358229b42a9fa2b092c5d3e38d3ff859c056
SimonMachhabhai <Simon_96m@hotmail.com>
2021-04-01 14:42:56 +0200

    Some changes

--- Source/LudoBoard/DataModels/Game.cs
+++ Source/LudoBoard/DataModels/Game.cs
@@ -6,12 +6,81 @@ using System.Threading.Tasks;
 
 namespace LudoBoard.DataModels
 {
-	class Game
+
+	public class Game
 	{
-		public int Id { get; set; }
+        private static int userInput = 0;
+        private static bool isRunning = true;
+
+
+        public int Id { get; set; }
 		public Piece Piece { get; set; }
 		public DateTime LastTimePlayedDate { get; set; } //Används vid laddning och sparning av spel
 		public DateTime CompletedDate { get; set; }
 		public bool IsCompleted { get; set; }
-	}
+
+
+        public static void CreateGame()
+        {
+            // Generate new game
+            Console.WriteLine("How many players? (2-4)");
+            List<Player> player = new List<Player>();
+
+            // Checking the amount of players
+            while (isRunning)
+            {
+                int.TryParse(Console.ReadLine(), out userInput);
+
+                if (userInput > 1 && userInput < 5)
+                {
+                    for (int i = 0; i < userInput; i++)
+                    {
+                        Console.WriteLine("Added a player to List");
+                        player.Add(new Player());
+                    }
+                    isRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+
+            List<string> colors = new List<string>() { "Red", "Blue", "Green", "Yellow" };
+            for (int i = 0; i < player.Count; i++)
+            {
+                // Försök att fixa ENUM
+                foreach (var color in Enum.GetValues(typeof(Color)))
+                {
+
+                }
+
+                //Sätter spelarens färg
+                player[i].PlayerColor = colors[i];
+                Console.WriteLine($"Player {i + 1} Name: ");
+
+                //Sätter spelarens namn
+                player[i].Name = Console.ReadLine().ToString();
+
+                Console.WriteLine($"Player {i + 1} Name: {player[i].Name} Color: {player[i].PlayerColor}");
+            }
+
+        }
+        public static void LoadGame()
+        {
+            //Laddar ett spel som man kan välja i en lista
+        }
+
+        public void CurrentBoard()
+        {
+
+        }
+
+        public void ContinueGame()
+        {
+            Game.LoadGame(); //Ladda senast sparade spelet
+        }
+
+    }
+
 }
