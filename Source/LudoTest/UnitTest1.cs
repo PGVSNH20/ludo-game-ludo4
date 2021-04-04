db1f18a3e6a45b74a94daf3b2a109f1c82bf527e
Magnusson <Magnusson-5@hotmail.com>
2021-04-04 10:02:09 +0200

    Added some information in Unit Tests, Just to get us started

--- Source/LudoTest/UnitTest1.cs
+++ Source/LudoTest/UnitTest1.cs
@@ -8,6 +8,27 @@ namespace LudoTest
         [Fact]
         public void Test1()
         {
+            // Arange - lägg upp testet genom att ta in variabler osv.
+
+            // Act - Vad ska testet göra, en metod som får ut ett resultat?
+
+            // Assert - Vad vill vi att resultatet ska bli?
+            
+        }
+
+        // EXEMPEL
+        // Så här kan man göra om man vill testa en funktion med flera olika inparametrar.
+        // Detta gör vi för att motverka redundans
+        [Theory]
+        [InlineData(5, 2, 3)]
+        [InlineData(0, 5, -5)]
+        public void Test2(int expected, int x, int y)
+        {
+            // Arange - lägg upp testet genom att ta in variabler osv.
+
+            // Act - Vad ska testet göra, en metod som får ut ett resultat?
+
+            // Assert - Vad vill vi att resultatet ska bli?
 
         }
     }
