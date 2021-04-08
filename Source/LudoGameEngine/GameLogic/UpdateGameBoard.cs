e90e5e4dd2c8d02383d67dbd95bf3f7e4fc8f1cd
Magnusson <Magnusson-5@hotmail.com>
2021-04-08 08:14:52 +0200

    Got method PiecesOnGameBoardUpdate to work

--- Source/LudoGameEngine/GameLogic/UpdateGameBoard.cs
+++ Source/LudoGameEngine/GameLogic/UpdateGameBoard.cs
@@ -74,146 +74,121 @@ namespace LudoGameEngine.GameLogic
         // TODO - PRIORITERING, FÅ DENNA ATT FUNGERA. Den skall uppdatera spelbrädet
         public static List<string> PiecesOnGameBoardUpdate(List<Piece> allActivePieces, List<Player> allActivePlayers)
         {
-
+            LudoDbAccess ludoDbAccess = new LudoDbAccess();
             List<string> gb = new GameBoard().CompleteGameBoard;
 
-            // i = 60
-            for (int i = 0; i < gb.Count; i++)
+            // Tar ut alla spelares pjäser till 4 olika listor 0 = spelare 1, 1 = spelare 2 osv.
+            List<List<Piece>> PlayersGameBoards = new List<List<Piece>>();
+            for (int i = 0; i < allActivePlayers.Count; i++)
             {
-                int positionCounter = 0;
-
-
-                foreach (var piece in allActivePieces)
-                {
-                    //if (piece.PlayerId == allActivePlayers[x].Id)
-                    //{
-
-                    if (piece.Position == i)
-                    {
-                        positionCounter++;
-                    }
-
-
-                }
-
-
-                for (int x = 0; x < allActivePlayers.Count; x++)
-                {
-                    foreach (var piece in allActivePieces)
-                    {
-                        if (piece.Position == i)
-                        {
-                            if (piece.PlayerId == allActivePlayers[x].Id)
-                            {
-                                if (x == 0)
-                                {
-                                    for (int u = 0; u < positionCounter; u++)
-                                    {
-                                        gb[i] += "1";
-                                    }
-                                    for (int l = positionCounter; l < 4; l++)
-                                    {
-                                        gb[i] += " ";
-                                    }
-                                    break;
-                                }
-                                if (x == 1)
-                                {
-                                    for (int u = 0; u < positionCounter; u++)
-                                    {
-                                        gb[i] += "2";
-                                    }
-                                    for (int l = positionCounter; l < 4; l++)
-                                    {
-                                        gb[i] += " ";
-                                    }
-                                    break;
-                                }
-                                if (x == 2)
-                                {
-                                    for (int u = 0; u < positionCounter; u++)
-                                    {
-                                        gb[i] += "3";
-                                    }
-                                    for (int l = positionCounter; l < 4; l++)
-                                    {
-                                        gb[i] += " ";
-                                    }
-                                    break;
-                                }
-                                if (x == 3)
-                                {
-                                    for (int u = 0; u < positionCounter; u++)
-                                    {
-                                        gb[i] += "4";
-                                    }
-                                    for (int l = positionCounter; l < 4; l++)
-                                    {
-                                        gb[i] += " ";
-                                    }
-                                    break;
-                                }
-                                
-                                break;
-                            }
-                            //else
-                            //{
-                            //    int ii = 4 - positionCounter;
-                            //    List<string> mellanrum = new List<string>(){
-                            //        " ", "  ","   ","    " };
-
-                            //    gb[i] += mellanrum[ii];
-                            //}
-                        }
- 
-                    }
-                }
+                PlayersGameBoards.Add(ludoDbAccess.GetCurrentPlayersPieces(allActivePlayers[i].Id));
             }
 
+            // Går igenom varje fyrkant på kartan
+            for (int i = 0; i < gb.Count; i++)
+            {
+                // Sätter en counter (Kan bara vara max 4 pieces på en ruta)
+                int counter = 4;
 
+                for (int x = 0; x < PlayersGameBoards.Count; x++) // Går igenom varje spelare
+                {
+                    int pieceCounter = 0; // Värdet på denna counter är id 1, 2 ,3 ,4 på en spelares pjäser.
+                                          // Detta för att vi ska kunna skriva ut "1", "2", "3" eller "4" på kartan.
 
+                    foreach (var piece in PlayersGameBoards[x]) // Går igenom varje spelares pjäser (4st)
+                    {
+                        pieceCounter += 1;
 
+                        if (piece.Position == i)
+                        {
+                            counter -= 1;
 
+                            if (x == 0) // Player 1
+                            {
+                                if (pieceCounter == 1) // piece Id
+                                {
+                                    gb[i] = gb[i] + "1";
+                                }
+                                else if (pieceCounter == 2)
+                                {
+                                    gb[i] = gb[i] + "2";
+                                }
+                                else if (pieceCounter == 3)
+                                {
+                                    gb[i] = gb[i] + "3";
+                                }
+                                else if (pieceCounter == 4)
+                                {
+                                    gb[i] = gb[i] + "4";
+                                }
+                            }
+                            else if (x == 1) // Player 2
+                            {
+                                if (pieceCounter == 1) // piece Id
+                                {
+                                    gb[i] = gb[i] + "1";
+                                }
+                                else if (pieceCounter == 2)
+                                {
+                                    gb[i] = gb[i] + "2";
+                                }
+                                else if (pieceCounter == 3)
+                                {
+                                    gb[i] = gb[i] + "3";
+                                }
+                                else if (pieceCounter == 4)
+                                {
+                                    gb[i] = gb[i] + "4";
+                                }
+                            }
+                            else if (x == 2) // Player 3
+                            {
+                                if (pieceCounter == 1) // piece Id
+                                {
+                                    gb[i] = gb[i] + "1";
+                                }
+                                else if (pieceCounter == 2)
+                                {
+                                    gb[i] = gb[i] + "2";
+                                }
+                                else if (pieceCounter == 3)
+                                {
+                                    gb[i] = gb[i] + "3";
+                                }
+                                else if (pieceCounter == 4)
+                                {
+                                    gb[i] = gb[i] + "4";
+                                }
+                            }
+                            else if (x == 3) // Player 4
+                            {
+                                if (pieceCounter == 1) // piece Id
+                                {
+                                    gb[i] = gb[i] + "1";
+                                }
+                                else if (pieceCounter == 2)
+                                {
+                                    gb[i] = gb[i] + "2";
+                                }
+                                else if (pieceCounter == 3)
+                                {
+                                    gb[i] = gb[i] + "3";
+                                }
+                                else if (pieceCounter == 4)
+                                {
+                                    gb[i] = gb[i] + "4";
+                                }
+                            }
+                        }
+                    }
+                }
 
-
-            //if (piece.PlayerId == allActivePlayers[x].Id)
-            //{
-            //    if (x == 0)
-            //    {
-            //        Console.WriteLine("PositionCounter" + positionCounter);
-
-            //        string p = "1";
-            //        for (int y = 0; y < positionCounter; y++)
-            //        {
-            //            p += "1";
-            //        }
-
-            //        gb[i] = gb[i].Replace(" ", "1");
-            //        break;
-            //    }
-            //    if (x == 1)
-            //    {
-            //        gb[i] = gb[i].Replace(" ", "2");
-            //        break;
-            //    }
-            //    if (x == 2)
-            //    {
-            //        gb[i] = gb[i].Replace(" ", "3");
-            //        break;
-            //    }
-            //    if (x == 3)
-            //    {
-            //        gb[i] = gb[i].Replace(" ", "4");
-            //        break;
-            //    }
-
-            //    break;
-            //}
-
-            //Console.WriteLine($"MapIndex: {i}");
-
-
-            //}
+                while (counter != 0)
+                {
+                    gb[i] = gb[i] + " ";
+                    counter -= 1;
+                }
+            }
 
             return gb;
         }
